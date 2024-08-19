4cf21b69c213f40843f372e980176a7146edd701
Ricardo Teófilo Esteve Hiyelekwa <ricardo.hiyelekwa@minfin.gov.ao>
2024-08-19 11:33:15 +0100

    aprendizagem do next js

---
+++ src/pages/contacto.tsx
@@ -0,0 +1,9 @@
+
+export default function Contacto() {
+    return (
+      <div>
+        <h1>Página de Contactos</h1>
+      </div>
+    );
+  }
+  
---
+++ src/pages/sobre.tsx
@@ -0,0 +1,10 @@
+
+export default function Sobre() {
+    return (
+      <div>
+        <h1>Página Sobre</h1>
+
+      </div>
+    );
+  }
+  
